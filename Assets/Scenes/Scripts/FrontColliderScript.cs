aeaa7d5a13914021fcb66261e7f65796a07c912a
Guanlin Chen <lolitsundere@hotmail.com>
2018-05-24 02:53:40 +0800

    Initial commit

---
+++ Assets/Scenes/Scripts/FrontColliderScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontColliderScript : MonoBehaviour {
+
+    internal GameObject TresureBox;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "TresureBox")
+        {
+            TresureBox = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.Equals(TresureBox))
+        {
+            TresureBox = null;
+        }
+    }
+}
---
+++ Assets/Scenes/Scripts/TresureBoxController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TresureBoxController : MonoBehaviour {
+
+    private Transform top;
+
+	void Start ()
+    {
+        top = transform.GetChild(0);	
+	}
+	
+    public void OpenBox()
+    {
+        StartCoroutine("Open");
+    }
+
+    private IEnumerator Open()
+    {
+        yield return new WaitForSeconds(0.5f);
+        while (top.rotation.eulerAngles.x > -60)
+        {
+            top.rotation = Quaternion.Euler(top.rotation.eulerAngles.x - 2, top.rotation.eulerAngles.y, top.rotation.eulerAngles.z);
+            yield return 0;
+        }
+        yield return new WaitForSeconds(1f);
+
+    }
+}
