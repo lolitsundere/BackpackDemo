fc247db0a82fb9f5e354d15a24d960f1316bd2d3
Guanlin Chen <lolitsundere@hotmail.com>
2018-05-25 02:53:24 +0800

    Add backpack UI

--- Assets/Scenes/Scripts/PackpackIconScript.cs
+++ Assets/Scenes/Scripts/PackpackIconScript.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class PackpackIconScript : MonoBehaviour
 {
-    UIButton button;
     TweenScale tweenScale;
+    GameObject backpackContainer;
     private void Start()
     {
-        button = transform.GetComponent<UIButton>();
         tweenScale = transform.GetComponent<TweenScale>();
-        button.onClick.Add(new EventDelegate(()=>
-        {
-            tweenScale.enabled = true;
-        }));
+        backpackContainer = GameObject.Find("BackpackContainer");
+        backpackContainer.SetActive(false);
     }
     
+    public void PlayTween()
+    {
+        tweenScale.enabled = true;
+    }
 
+    public void StopTween()
+    {
+        tweenScale.enabled = false;
+    }
+
+    public void TurnBackpack()
+    {
+        backpackContainer.SetActive(!backpackContainer.activeSelf);
+    }
 }
